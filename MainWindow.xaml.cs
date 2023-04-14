7685e658447441a617bcf1b71fb492712ebf560a
shane morrison <sorrison@us.ibm.com>
2023-04-14 13:09:38 -0400

    up

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Sockets;
+using System.Numerics;
 using System.Reflection.Metadata;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -28,7 +30,50 @@ namespace SoftwareRayTrace
         WriteableBitmap writeableBitmap;
         MipArray mipArray;
         int curLod = 0;
+        struct Ray
+        {
+            public Ray(Vector3 p, Vector3 d)
+            {
+                pos = p;
+                dir = Vector3.Normalize(d);
+            }
 
+            public Vector3 AtT(float t)
+            {
+                return pos + t * dir;
+            }
+            public Vector3 pos;
+            public Vector3 dir;
+        }
+
+        struct Plane
+        {
+            public Plane(float _d, Vector3 _n)
+            {
+                d = _d;
+                nrm = Vector3.Normalize(_n);
+            }
+
+            public Vector3 nrm;
+            public float d;
+
+            public float Intersect(Ray r)
+            {
+                return -(Vector3.Dot(r.pos, nrm) + d) / Vector3.Dot(r.dir, nrm);
+            }
+        }
+
+        static Plane[] SidePlanes =
+        {
+            new Plane(0, new Vector3(1,0,0)),
+            new Plane(-1, new Vector3(-1,0,0)),
+            new Plane(0, new Vector3(0,1,0)),
+            new Plane(-1, new Vector3(0,-1,0)),
+            new Plane(0, new Vector3(0,0,1)),
+            new Plane(-1, new Vector3(0,0,-1)),
+        };
+
+        Ray curRay;
         public MainWindow()
         {
             InitializeComponent();
@@ -40,96 +85,164 @@ namespace SoftwareRayTrace
             96,
             PixelFormats.Bgr32,
             null);
+            
+            RenderOptions.SetBitmapScalingMode(this.topDownView, BitmapScalingMode.NearestNeighbor);
+            RenderOptions.SetEdgeMode(this.topDownView, EdgeMode.Aliased);
+            this.topDownView.Source = writeableBitmap;
 
-            RenderOptions.SetBitmapScalingMode(this.img, BitmapScalingMode.NearestNeighbor);
-            RenderOptions.SetEdgeMode(this.img, EdgeMode.Aliased);
-            this.img.Source = writeableBitmap;
-
-            img.Stretch = Stretch.Uniform;
-            img.HorizontalAlignment = HorizontalAlignment.Left;
-            img.VerticalAlignment = VerticalAlignment.Top;
-
-            mipArray = LoadPng("na.png");
+            topDownView.Stretch = Stretch.Uniform;
+            topDownView.HorizontalAlignment = HorizontalAlignment.Left;
+            topDownView.VerticalAlignment = VerticalAlignment.Top;
+            mipArray = MipArray.LoadPng("na.png");
             for (int i = 0; i < mipArray.mips.Length; i++)
             {
                 this.LODCb.Items.Add(i.ToString());
             }
-            DrawTiles();
+            CastRay(new Ray(new Vector3(0.7f, 1.0f, 0.5f), new Vector3(-0.2f, -1, 0)));
+            Repaint();
         }
 
-        
-
-        MipArray LoadPng(string f)
+        int RGBToI(byte R, byte G, byte B)
         {
-            // Open a Stream and decode a PNG image
-            Stream imageStreamSource = new FileStream(f, FileMode.Open, FileAccess.Read, FileShare.Read);
-            PngBitmapDecoder decoder = new PngBitmapDecoder(imageStreamSource, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
-            BitmapSource bitmapSource = decoder.Frames[0];
-            byte[] pixels = new byte[(int)bitmapSource.Width * (int)bitmapSource.Height * 4];
-            bitmapSource.CopyPixels(pixels, 256 * 4, 0);
-            ulong maxVal = 0;
-            ulong minVal = ulong.MaxValue;
-            for (int i = 0; i < 256*256; ++i)
-            {
-                ulong b = pixels[i * 4];
-                ulong g = pixels[i * 4 + 1];
-                ulong r = pixels[i * 4 + 2];
-                ulong eval = r * 256 * 256 + g * 256 + b;
-                maxVal = Math.Max(maxVal, eval);
-                minVal = Math.Min(minVal, eval);
-            }
-            ulong range = maxVal - minVal;
-            byte []nrmVals = new byte[256 * 256 * 2];
-            for (int i = 0; i < 256 * 256; ++i)
-            {
-                ulong b = pixels[i * 4];
-                ulong g = pixels[i * 4 + 1];
-                ulong r = pixels[i * 4 + 2];
-                ulong eval = r * 256 * 256 + g * 256 + b;
-                ushort nrmval = (ushort)(((eval - minVal) * ushort.MaxValue) / range);
-                nrmVals[i * 2] = (byte)(nrmval & 0xFF);
-                nrmVals[i * 2 + 1] = (byte)(nrmval >> 8);
-            }
-            nint ptr = Marshal.AllocHGlobal(nrmVals.Length);
-            Marshal.Copy(nrmVals, 0, ptr, nrmVals.Length);
-            return new MipArray(new Mip { data = ptr, width = 256 });
+            int color_data = R << 16; // R
+            color_data |= G << 8;   // G
+            color_data |= B << 0;   // B
+
+            return color_data;
         }
 
+        void DrawRayLine()
+        {
+            float mint = float.MaxValue;
+            int minPlane = -1;
+            for (int i = 0; i < SidePlanes.Length; ++i)
+            {
+                float t = SidePlanes[i].Intersect(this.curRay);
+                if (t > 0 && t < mint)
+                {
+                    minPlane = i;
+                    mint = t;
+                }
+            }
+            Vector3 endPt = this.curRay.AtT(mint);
+            DrawLine(new Vector2(this.curRay.pos.X, this.curRay.pos.Y), new Vector2(endPt.X, endPt.Y), RGBToI(255, 255, 0));
 
-        void DrawTiles()
+        }
+        void Repaint()
         {
             if (writeableBitmap == null)
                 return;
             writeableBitmap.Lock();
-            unsafe
-            {
-                // Get a pointer to the back buffer.
-                nint pBackBuffer = writeableBitmap.BackBuffer;
-                for (int y = 0; y < 256; y++)
-                {
-                    nint pRowPtr = pBackBuffer + y * writeableBitmap.BackBufferStride;
-                    for (int x = 0; x < 256; x++)
-                    {
-                        ushort val = mipArray.SampleLod((x + 0.5f) / 256.0f , (y + 0.5f) / 256.0f, curLod);
-                        int ival = (int)val >> 8;
-                        // Compute the pixel's color.
-                        int color_data = ival << 16; // R
-                        color_data |= ival << 8;   // G
-                        color_data |= ival << 0;   // B
+            DrawTiles();
+            DrawRayLine();
+            writeableBitmap.AddDirtyRect(new Int32Rect(0, 0, 256, 256));
+            writeableBitmap.Unlock();
+        }
 
-                        *((int*)pRowPtr) = color_data;
-                        pRowPtr += 4;
+
+
+        unsafe void DrawTiles()
+        {
+            // Get a pointer to the back buffer.
+            nint pBackBuffer = writeableBitmap.BackBuffer;
+            for (int y = 0; y < 256; y++)
+            {
+                nint pRowPtr = pBackBuffer + y * writeableBitmap.BackBufferStride;
+                for (int x = 0; x < 256; x++)
+                {
+                    float val = mipArray.SampleLod((x + 0.5f) / 256.0f , (y + 0.5f) / 256.0f, curLod);                    
+                    *((int*)pRowPtr) = RGBToI((byte)(val * 255.0f), (byte)(val * 255.0f), (byte)(val * 255.0f));
+                    pRowPtr += 4;
+                }
+            }
+        }
+
+        unsafe void DrawLine(Vector2 start, Vector2 end, int color)
+        {
+            nint pBackBuffer = writeableBitmap.BackBuffer;
+            Vector2 dir = end - start;
+            if (Math.Abs(dir.X) > Math.Abs(dir.Y))
+            {
+                int yStart = (int)(start.Y * 256);
+                int xStart = (int)(start.X * 256);
+                int yEnd = (int)(end.Y * 256);
+                int xEnd = (int)(end.X * 256);
+                if (yStart > yEnd)
+                {
+                    int tmp = xStart;
+                    xStart = xEnd;
+                    xEnd = tmp;
+                    tmp = yStart;
+                    yStart = yEnd;
+                    yEnd = tmp;
+                }
+
+                bool ishorizontal = dir.Y == 0;
+                float slope = dir.X / dir.Y;
+                for (int y = yStart; y <= yEnd; ++y)
+                {
+                    int xs = ishorizontal ? xStart : (int)((y - yStart) * slope) + xStart;
+                    int xe = ishorizontal ? xEnd : (int)((y - yStart + 1) * slope) + xStart;
+                    nint pRowPtr = pBackBuffer + y * writeableBitmap.BackBufferStride;
+                    for (int x = xs; x < xe; ++x)
+                    {
+                        if (y < 0 || y > 255 || x < 0 || x > 255)
+                            continue;
+                        nint pCur = pRowPtr + x * 4;
+                        *((int*)pCur) = color;
                     }
                 }
             }
-            writeableBitmap.AddDirtyRect(new Int32Rect(0, 0, 256, 256));
-            writeableBitmap.Unlock();
+            else
+            {
+                int yStart = (int)(start.Y * 256);
+                int yEnd = (int)(end.Y * 256);
+                int xStart = (int)(start.X * 256);
+                int xEnd = (int)(end.X * 256);
+                float slope = dir.Y / dir.X;
+                if (xStart > xEnd)
+                {
+                    int tmp = xStart;
+                    xStart = xEnd;
+                    xEnd = tmp;
+                    tmp = yStart;
+                    yStart = yEnd;
+                    yEnd = tmp;
+                }
+                bool isvertical = dir.X == 0;
+                for (int x = xStart; x <= xEnd; ++x)
+                {
+                    int ys = isvertical ? Math.Min(yStart, yEnd) : (int)((x - xStart) * slope) + yStart;
+                    int ye = isvertical ? Math.Max(yEnd, yStart) : (int)((x - xStart + 1) * slope) + yStart;
+                    for (int y = ys; y < ye; ++y)
+                    {
+                        if (y < 0 || y > 255 || x < 0 || x > 255)
+                            continue;
+                        nint pRowPtr = pBackBuffer + y * writeableBitmap.BackBufferStride;
+                        nint pCur = pRowPtr + x * 4;
+                        *((int*)pCur) = color;
+                    }
+                }
+
+            }
+        }
+
+
+        void CastRay(Ray ray)
+        {            
+            this.curRay = ray;
+            
+            for (int lod = mipArray.mips.Length - 1; lod >= 0; --lod)
+            {
+                Mip mip = mipArray.mips[lod];
+                Vector2 pixelSize = new Vector2(1.0f / mip.width, 1.0f / mip.height);
+            }
         }
 
         private void LODCb_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             curLod = int.Parse((string)this.LODCb.SelectedItem);
-            DrawTiles();
+            Repaint();
         }
     }
 }
--- Mip.cs
+++ Mip.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
+using System.IO;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 
 namespace SoftwareRayTrace
 {
@@ -11,12 +14,15 @@ namespace SoftwareRayTrace
     {
         public nint data;
         public int width;
+        public int height;
+        const float mult = 1.0f / (float)ushort.MaxValue;
 
-        public unsafe ushort Sample(float u, float v)
+        public unsafe float Sample(float u, float v)
         {
             int iu = (int)(u * width);
-            int iv = (int)(v * width);
-            return *((ushort *)(data + (iv * width + iu) * sizeof(ushort)));
+            int iv = (int)(v * height);
+            ushort sval = *((ushort *)(data + (iv * width + iu) * sizeof(ushort)));
+            return (float)sval * mult;
         }
     }
 
@@ -34,7 +40,42 @@ namespace SoftwareRayTrace
             }
         }
 
-        public ushort SampleLod(float u, float v, int lod)
+        public static MipArray LoadPng(string f)
+        {
+            // Open a Stream and decode a PNG image
+            Stream imageStreamSource = new FileStream(f, FileMode.Open, FileAccess.Read, FileShare.Read);
+            PngBitmapDecoder decoder = new PngBitmapDecoder(imageStreamSource, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
+            BitmapSource bitmapSource = decoder.Frames[0];
+            byte[] pixels = new byte[(int)bitmapSource.Width * (int)bitmapSource.Height * 4];
+            bitmapSource.CopyPixels(pixels, 256 * 4, 0);
+            ulong maxVal = 0;
+            ulong minVal = ulong.MaxValue;
+            for (int i = 0; i < 256 * 256; ++i)
+            {
+                ulong b = pixels[i * 4];
+                ulong g = pixels[i * 4 + 1];
+                ulong r = pixels[i * 4 + 2];
+                ulong eval = r * 256 * 256 + g * 256 + b;
+                maxVal = Math.Max(maxVal, eval);
+                minVal = Math.Min(minVal, eval);
+            }
+            ulong range = maxVal - minVal;
+            byte[] nrmVals = new byte[256 * 256 * 2];
+            for (int i = 0; i < 256 * 256; ++i)
+            {
+                ulong b = pixels[i * 4];
+                ulong g = pixels[i * 4 + 1];
+                ulong r = pixels[i * 4 + 2];
+                ulong eval = r * 256 * 256 + g * 256 + b;
+                ushort nrmval = (ushort)(((eval - minVal) * ushort.MaxValue) / range);
+                nrmVals[i * 2] = (byte)(nrmval & 0xFF);
+                nrmVals[i * 2 + 1] = (byte)(nrmval >> 8);
+            }
+            nint ptr = Marshal.AllocHGlobal(nrmVals.Length);
+            Marshal.Copy(nrmVals, 0, ptr, nrmVals.Length);
+            return new MipArray(new Mip { data = ptr, width = 256, height = 256 });
+        }
+        public float SampleLod(float u, float v, int lod)
         {
             return mips[lod].Sample(u, v);
         }
@@ -42,9 +83,10 @@ namespace SoftwareRayTrace
         {
             Mip mipD = new Mip();
             mipD.width = mip.width / 2;
+            mipD.height = mip.height / 2;
 
-            nint data = Marshal.AllocHGlobal(mipD.width * mipD.width * 2);
-            for (int y = 0; y < mipD.width; ++y)
+            nint data = Marshal.AllocHGlobal(mipD.width * mipD.height * 2);
+            for (int y = 0; y < mipD.height; ++y)
             {
                 nint outRow = data + (y * mipD.width * sizeof(ushort));
                 nint inRow0 = mip.data + (y * 2 * mip.width * sizeof(ushort));
